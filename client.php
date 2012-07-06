dbf7c3b131cd1e8ff2934d33652aaa4094ff8ca7
Daniel Jost <info@daniel-jost.de>
2012-07-06 21:53:05 +0200

    - changed class architecture

--- client.php
+++ client.php
@@ -6,10 +6,18 @@ error_reporting(E_ALL);
 require_once('library/EA/AutoLoader.php');
 EA_AutoLoader::init(array(), 'library/');
 
-$oCheck = new EA_Check_Http();
-$oCheck->setHost('www.google.de');
-$oCheck->setPort(80);
-$oCheck->setSsl(false);
+$oCheck2 = new EA_Check_Http_Request();
+$oCheck2->setHost('www.google.de');
+$oCheck2->setPort(80);
+$oCheck2->setSsl(false);
+
+$oRequest2 = new EA_Request();
+$oRequest2->setCheck($oCheck2);
+
+$oCheck = new EA_Check_Remote_Request();
+$oCheck->setRemoteHost('192.168.2.108');
+$oCheck->setRemotePort(9786);
+$oCheck->setRequest($oRequest2);
 
 $oRequest = new EA_Request();
 $oRequest->setCheck($oCheck);
